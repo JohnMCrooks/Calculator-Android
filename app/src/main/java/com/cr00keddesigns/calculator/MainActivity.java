f408b00428304a49d312bb610877b037f6b4f91d
John Crooks <johnmcrooks@gmail.com>
2016-08-28 14:54:29 -0400

    Found and implemented code to Evaluate In-line expressions.
    TODO:
    Deal with Modulo (Alter code to include it or remove it all together)
    Figure out how to add TickerTape (previous calculations)
    Polish

--- app/src/main/java/com/cr00keddesigns/calculator/MainActivity.java
+++ app/src/main/java/com/cr00keddesigns/calculator/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtResult;
 
     private EditText operand1;
-    private EditText operand2;
 
     private Button btnAddition;
     private Button btnSubtraction;
@@ -66,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         btn8 = (Button) findViewById(R.id.btn8);
         btn9 = (Button) findViewById(R.id.btn9);
 
-
         txtResult = (TextView) findViewById(R.id.txtResult);
 
         //on click listener allows us to click the button and cause an action to occur
@@ -75,10 +73,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!operand1.equals(null)){
+                    double result = eval(operand1.getText().toString());
+                    txtResult.setText(Double.toString(result));
 
+                    //TODO for the sake of remembering... http://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form
                 }
             }
         });
+
+
 
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -200,65 +203,91 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        btnAddition.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(operand1.getText().length()>0 && (operand2.getText().length()>0)){
-//                    float op1 = Float.parseFloat(operand1.getText().toString());
-//                    float op2 = Float.parseFloat((operand2.getText().toString()));
-//                    double theResult = op1;
-//                    txtResult.setText(Double.toString(theResult));
-//                }
-//                else{
-//                    Toast.makeText(MainActivity.this, "Hey Hey Hey! Use some numbers dag nabit!", Toast.LENGTH_LONG).show();}
-//            }
-//        });
-
-//
-//        btnSubtraction.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                double op1 = Double.parseDouble(operand1.getText().toString());
-//                double op2 = Double.parseDouble((operand2.getText().toString()));
-//                double theResult = op1 - op2;
-//                txtResult.setText(Double.toString(theResult));
-//            }
-//        });
-//
-//        btnMultip.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                double op1 = Double.parseDouble(operand1.getText().toString());
-//                double op2 = Double.parseDouble((operand2.getText().toString()));
-//                double theResult = op1*op2;
-//                txtResult.setText(Double.toString(theResult));
-//            }
-//        });
-//
-//        btnModulo.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                double op1 = Double.parseDouble(operand1.getText().toString());
-//                double op2 = Double.parseDouble((operand2.getText().toString()));
-//                int theResult = (int) (op1%op2);
-//                txtResult.setText(Double.toString(theResult));
-//            }
-//        });
-//
-//        btnDivision.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                double op1 = Double.parseDouble(operand1.getText().toString());
-//                double op2 = Double.parseDouble((operand2.getText().toString()));
-//                double theResult = op1/op2;
-//                txtResult.setText(Double.toString(theResult));
-//            }
-//        });
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }
 
+/* The Evaluation Code below was found on Stack Overflow originally posted by User Boann.
+Thread: http://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form     */
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
     }
 
     @Override
